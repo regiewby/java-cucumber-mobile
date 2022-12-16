d3f7504e49d9d108a3adf922c82f6ce921293fa3
regiewby <regiewby@gmail.com>
2022-12-16 20:57:27 +0700

    first commit

---
+++ src/test/java/app/bersama/pages/MyAccountPage.java
@@ -0,0 +1,36 @@
+package app.bersama.pages;
+
+import app.bersama.DriverManager;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+/**
+ * @author regiewby on 16/12/22
+ * @project java-cucumber-mobile
+ */
+public class MyAccountPage {
+
+    private AppiumDriver appiumDriver;
+
+    public MyAccountPage(AppiumDriver driver) {
+        this.appiumDriver = driver;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "id.binar.fp.secondhand:id/btn_login")
+    private WebElement button_login;
+
+    public void tapButtonLogin() {
+        WebDriverWait wait = new WebDriverWait(DriverManager.getInstance().getDriver(), Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOf(button_login));
+        button_login.click();
+    }
+}
---
+++ src/test/java/app/bersama/pages/NavigationSectionPage.java
@@ -0,0 +1,46 @@
+package app.bersama.pages;
+
+import app.bersama.DriverManager;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author regiewby on 16/12/22
+ * @project java-cucumber-mobile
+ */
+public class NavigationSectionPage {
+
+    private AppiumDriver appiumDriver;
+
+    public NavigationSectionPage(AppiumDriver driver) {
+        this.appiumDriver = driver;
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "id.binar.fp.secondhand:id/navigation_profile")
+    private WebElement button_account;
+
+    @FindBy(id = "id.binar.fp.secondhand:id/navigation_history")
+    private WebElement button_transaction;
+
+    @FindBy(id = "id.binar.fp.secondhand:id/navigation_notification")
+    private WebElement button_notification;
+
+    @FindBy(id = "id.binar.fp.secondhand:id/navigation_home")
+    private WebElement button_home;
+
+    public void tapNavigationAccount() {
+        WebDriverWait wait = new WebDriverWait(DriverManager.getInstance().getDriver(), Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOf(button_account));
+        button_account.click();
+    }
+}
